cad55c51ff833d202920db1a79b49f8f809b4620
Garazbolg <legall.joseph@laposte.net>
2019-09-20 16:38:55 +0200

    720p to 1080p

--- Run/1-SimulatorUnreal.bat
+++ Run/1-SimulatorUnreal.bat
@@ -1,2 +1,2 @@
 cd C:\Users\Megaport\eSoftThings\carla\Dist\Latest\WindowsNoEditor
-CarlaUE4 ResX=3840 ResY=720 NoVSync USEALLAVAILABLECORES NOSOUND
+CarlaUE4 ResX=5760 ResY=1080 NoVSync USEALLAVAILABLECORES NOSOUND
